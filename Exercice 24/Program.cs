b2c27c734d739193bc66eac09b49e1eaf46db3e6
Akyuma <Akyuma@DESKTOP-GRJFV4I>
2024-02-22 21:06:47 +0100

    Ajoutez des fichiers projet.

---
+++ Exercice 24/Program.cs
@@ -0,0 +1,42 @@
+﻿/**
+ * Exercice 24
+ * author : Mallory P
+ * date : 21/02/2024
+ */
+
+using System;
+
+namespace Exercice_24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double nbGrand, nbPetit;
+
+            Console.Write("Saisissez une température : ");           
+            nbGrand=nbPetit= double.Parse(Console.ReadLine());
+
+            for (int i = 1; i < 10; i++)
+            {
+                Console.Write("Saisissez une température : ");
+                double compte = double.Parse(Console.ReadLine());
+                if (compte > nbGrand)
+                {
+                    nbGrand = compte;
+                }
+                if (compte < nbPetit)
+                {
+                    nbPetit = compte;
+                }
+                else
+                {
+                    nbGrand = compte;
+                    nbPetit = compte;
+                }
+            }
+            Console.Write("la température la plus petite est : " + nbPetit + " et la température la plus grande : " + nbGrand);
+            Console.ReadLine();
+        }
+    }
+}
